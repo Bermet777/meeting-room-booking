a4de8bdfa9b9cb91efa2bbca7f5d4a143107e097
bermet <bermet.alibek@gmail.com>
2024-04-12 17:37:35 -0500

    message changed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -39,7 +39,7 @@ const RoomList = () => {
   return (
     <div>
       <h2>Available Rooms</h2>
-      <p>Select a date from calendar to view available room</p>
+      <p>Select a date from calendar to view available rooms</p>
       <input
         type="date"
         value={selectedDate}
